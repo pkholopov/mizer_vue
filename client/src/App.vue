f58b987a1b7022eaffc6d584a553e97274c44e0a
Pavel Kholopov <ironface13@gmail.com>
2024-03-21 22:17:36 +0300

    feature: save the color scheme after refreshing the page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,34 @@
 <template>
-  <ThemeSwitcher class="absolute top-1 right-1 z-10"/>
+  <ThemeSwitcher class="absolute top-1 right-1 z-10" @toggle="toggle" :is-dark="isDark"/>
   <RouterView />
 </template>
 
 <script setup>
 import ThemeSwitcher from './components/ThemeSwitcher.vue';
+
+import { ref, onMounted, onBeforeMount } from 'vue';
+
+const isDark = ref(false)
+
+const toggle = () => {
+  isDark.value = !isDark.value
+  localStorage.setItem('theme', isDark.value ? 'dark' : 'light')
+  setupTheme()
+}
+
+const setupTheme = () => {
+  if(isDark.value) {
+    document.body.classList.add('dark')
+  } else {
+    document.body.classList.remove('dark')
+  }
+}
+
+onBeforeMount(() => {
+  isDark.value = localStorage.getItem('theme') === 'dark'
+})
+
+onMounted(() => {
+  setupTheme()
+})
 </script>
--- client/src/components/ThemeSwitcher.vue
+++ client/src/components/ThemeSwitcher.vue
@@ -1,19 +1,14 @@
 <template>
-  <button @click="toggleMode" class="bg-gray-200 dark:bg-gray-700 font-bold py-2 px-4 rounded dark:text-gray-300">{{ title }}</button>
+  <button @click="$emit('toggle')" class="bg-gray-200 dark:bg-gray-700 font-bold py-2 px-4 rounded dark:text-gray-300">{{ title }}</button>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
-const title = ref('Темная тема')
+const prop = defineProps({
+  isDark: Boolean
+})
 
-const toggleMode = () => {
-  if (document.body.classList.contains('dark')) {
-    document.body.classList.remove('dark')
-    title.value = 'Темная тема'
-  } else {
-    document.body.classList.add('dark')
-    title.value = 'Светлая тема'
-  }
-}
+const title = computed(() => (prop.isDark ? 'Светлая тема' : 'Темная тема'))
+
 </script>
